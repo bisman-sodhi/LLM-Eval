c449276e1e86a1fb843e7f5f2209784fd89ac976
bisman-sodhi <sodhibisman@gmail.com>
2025-01-04 17:20:01 -0800

    lint control

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['fonts.googleapis.com'],
+  }
 };
 
 export default nextConfig;
